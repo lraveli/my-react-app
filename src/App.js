88fe21143f87b9bf1424f83fed5e22bee41781eb
Leandro Raveli <leandro.raveli@vitality.co.uk>
2018-08-21 16:54:50 +0100

    Module 66

--- src/App.js
+++ src/App.js
@@ -42,7 +42,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -64,17 +65,27 @@ class App extends Component {
             })}
           </div> 
       );
+
+      style.backgroundColor = 'red';
+    }
+
+    let classes = [];
+    if(this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if(this.state.persons.length <= 1) {
+      classes.push('bold');
     }
 
     return (
-      <div className="App">
-      <h1>React App</h1>
-      <p>This is really working</p>
-      <button 
-        style={style}
-        onClick={this.togglePersonsHandler}>Toggle Persons</button>
-      {persons}
-      </div>
+        <div className="App">
+        <h1>React App</h1>
+        <p className={classes.join(' ')}>This is really working</p>
+        <button 
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
+        </div>
     );
   }
 }
